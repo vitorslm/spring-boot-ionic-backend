42395a77904f18cb9ff9135e275f4109561001bb
Vitor Mukai <vitorslmuk@gmail.com>
2018-05-11 11:28:49 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/vitormukai/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.vitormukai.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vitormukai.cursomc.domain.Categoria;
+import com.vitormukai.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
